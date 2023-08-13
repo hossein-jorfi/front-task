3760a1e40ff5a4cf2bb55ad60402e658035a1abc
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-08-13 08:30:27 +0330

    Product Card created

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+type ProductCardProps = {
+  id: number;
+  image: string;
+  title: string;
+};
+
+const ProductCard = ({}: ProductCardProps) => {
+  return <div className="rounded-lg"></div>;
+};
+
+export default ProductCard;
